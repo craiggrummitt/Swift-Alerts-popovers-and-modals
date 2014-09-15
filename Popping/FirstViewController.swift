12a6b6c053ee53ec8466b0ab020f26f25d32f992
Craig Grummitt <craiggrummitt@gmail.com>
2014-09-14 19:27:31 -0500

    Initial Commit

---
+++ Popping/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Popping
+//
+//  Created by CraigGrummitt on 14/09/2014.
+//  Copyright (c) 2014 CraigGrummitt. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
